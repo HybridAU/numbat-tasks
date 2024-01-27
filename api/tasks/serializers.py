4f8a679a1abcd2ab07ae96265e26fc4be48d38b4
Michael Van Delft <michael@hybr.id.au>
2024-01-27 09:08:34 +0800

    Make the owner default

--- api/tasks/serializers.py
+++ api/tasks/serializers.py
@@ -7,7 +7,4 @@ class TaskSerializer(serializers.ModelSerializer):
         model = Task
         fields = ["id", "owner", "created", "updated", "text", "complete"]
 
-    owner = serializers.ReadOnlyField(source="owner.email")
-
-    def perform_create(self, serializer):
-        serializer.save(owner=self.request.user)
+    owner = serializers.HiddenField(default=serializers.CurrentUserDefault())
