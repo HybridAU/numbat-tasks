33931d50d7508ac4613a0746d2c32557ea5fa991
Michael Van Delft <michael@xo.tc>
2024-02-09 16:37:38 +0800

    Add task to list from list_pk

--- api/tasks/serializers.py
+++ api/tasks/serializers.py
@@ -1,5 +1,21 @@
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
+from rest_framework_nested.serializers import NestedHyperlinkedModelSerializer
 from tasks.models import List, Task
+
+
+class ParentListDefault:
+    requires_context = True
+
+    def __call__(self, serializer_field):
+        list_id = serializer_field.context["view"].kwargs["list_pk"]
+        parent_list = List.objects.filter(id=list_id).first()
+        if (
+            not parent_list
+            or parent_list.owner != serializer_field.context["request"].user
+        ):
+            raise ValidationError()
+        return parent_list
 
 
 class ListSerializer(serializers.ModelSerializer):
@@ -10,7 +26,9 @@ class ListSerializer(serializers.ModelSerializer):
     owner = serializers.HiddenField(default=serializers.CurrentUserDefault())
 
 
-class TaskSerializer(serializers.ModelSerializer):
+class TaskSerializer(NestedHyperlinkedModelSerializer):
     class Meta:
         model = Task
-        fields = ["id", "created", "updated", "text", "complete"]
+        fields = ["id", "list", "created", "updated", "text", "complete"]
+
+    list = serializers.HiddenField(default=ParentListDefault())
