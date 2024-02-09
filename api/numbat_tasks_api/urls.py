dc8b3618e259317980d379ebbaa1dea2bb8f3b04
Michael Van Delft <michael@hybr.id.au>
2024-02-09 16:37:38 +0800

    docker compose file to add database and caddy servers

--- api/numbat_tasks_api/urls.py
+++ api/numbat_tasks_api/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for numbat_notes_api project.
+URL configuration for numbat_tasks_api project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
