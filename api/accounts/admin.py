1725ac2eb7840a069f52ea8ccef6514e9212c074
Michael Van Delft <michael@hybr.id.au>
2024-02-09 16:37:38 +0800

    add user admin

--- api/accounts/admin.py
+++ api/accounts/admin.py
@@ -1,1 +1,33 @@
+from accounts.models import CustomUser
+from django import forms
 from django.contrib import admin
+from django.forms import TextInput
+
+
+class CustomUserAdminForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(CustomUserAdminForm, self).__init__(*args, **kwargs)
+        self.fields["password"].widget = TextInput(attrs={"type": "password"})
+
+
+@admin.register(CustomUser)
+class CustomUserAdminAdmin(admin.ModelAdmin):
+    form = CustomUserAdminForm
+    list_display = [
+        "email",
+        "first_name",
+        "last_name",
+        "is_active",
+    ]
+    fields = [
+        "email",
+        "password",
+        "first_name",
+        "last_name",
+        "is_superuser",
+        "is_staff",
+        "is_active",
+        "groups",
+        "user_permissions",
+    ]
+    search_fields = ["email"]
