725d5661f7011e5105d1c792d6509730f2dceec5
Michael Van Delft <michael.vandelft@lotterywest.wa.gov.au>
2024-12-30 16:08:48 +0800

    Remove comments

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -2,9 +2,6 @@ import RequireAuth from "@auth-kit/react-router/RequireAuth";
 import { lazy } from "react";
 import { createBrowserRouter } from "react-router-dom";
 
-// I'm pretty sure this is a false positive based on
-// https://github.com/ArnaudBarre/eslint-plugin-react-refresh/issues/25#issuecomment-1729071347
-/* eslint-disable react-refresh/only-export-components */
 const HomePage = lazy(() => import("./pages/HomePage/HomePage"));
 const SignIn = lazy(() => import("./pages/SignIn/SignIn"));
 
