4ed55bf01654badcb9bb39dbe3673c3b6c7cdbaa
Michael Van Delft <michael@xo.tc>
2024-12-30 16:08:48 +0800

    Remove comments

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,4 @@
 import react from "@vitejs/plugin-react-swc";
-// This is a false positive, vite is listed as a dev dependency
-// and eslint would like it to be a core dependency because we
-// are importing it here.
-/* eslint-disable import/no-extraneous-dependencies */
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
