72c001a0faac808fc2b19e1d33f1cb6689912856
Michael Van Delft <michael.vandelft@lotterywest.wa.gov.au>
2024-12-30 16:06:50 +0800

    Switch to biome and update

--- frontend/src/theme/theme.tsx
+++ frontend/src/theme/theme.tsx
@@ -1,9 +1,9 @@
-import { createTheme } from '@mui/material/styles';
+import { createTheme } from "@mui/material/styles";
 
 const theme = createTheme({
   typography: {
     button: {
-      textTransform: 'none',
+      textTransform: "none",
     },
   },
 });
