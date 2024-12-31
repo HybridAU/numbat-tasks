9ff20a9508b48bca462a80efea4227f38d1bc9c5
Michael Van Delft <michael.vandelft@lotterywest.wa.gov.au>
2025-01-01 00:13:13 +0800

    Basic task component

--- frontend/src/components/Task.tsx
+++ frontend/src/components/Task.tsx
@@ -1,7 +1,31 @@
-type TaskProps = {
-  text: string;
-};
+import { Checkbox, Stack, Typography } from "@mui/material";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import useAuthHeader from "react-auth-kit/hooks/useAuthHeader";
+import { type TaskDetails, updateTask } from "../api/tasks.ts";
 
-export default function Task({ text }: TaskProps) {
-  return <li>{text}</li>;
+export default function Task({ text, complete, id }: TaskDetails) {
+  const queryClient = useQueryClient();
+  const authHeader = useAuthHeader();
+  // TODO list id is hard coded to 1, build a list provider to give state / context...
+  const listId = 1;
+  const { mutate } = useMutation({
+    mutationFn: () => {
+      return updateTask({
+        authHeader: authHeader,
+        listId: listId,
+        taskId: id,
+        complete: !complete,
+        text: text,
+      });
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["tasks", listId] });
+    },
+  });
+  return (
+    <Stack direction="row" alignItems="center">
+      <Checkbox checked={complete} onClick={() => mutate()} />
+      <Typography>{text}</Typography>
+    </Stack>
+  );
 }
