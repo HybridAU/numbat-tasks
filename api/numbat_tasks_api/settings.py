f90c19f1a75072fb54caf3e57a3fa4a4890d7249
Michael Van Delft <michael@xo.tc>
2025-01-13 16:18:08 +0800

    Add example build settings

--- api/numbat_tasks_api/settings.py
+++ api/numbat_tasks_api/settings.py
@@ -21,10 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
+DEBUG = config("DEBUG", cast=bool, default=False)
 
 SECRET_KEY = config("SECRET_KEY")
 
-DEBUG = config("DEBUG", cast=bool, default=False)
+if not DEBUG and "insecure" in SECRET_KEY:
+    raise Exception("Using an insecure development key in production!")
 
 ALLOWED_HOSTS = config("ALLOWED_HOSTS", cast=Csv())
 
