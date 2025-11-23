2db5299e193cd350fac8889159427c4c6b5e3f10
Michael Van Delft <michael@xo.tc>
2025-11-23 16:49:58 +0800

    Ruff changes

--- api/tasks/utils.py
+++ api/tasks/utils.py
@@ -3,5 +3,5 @@ def truncate(text: str, length: int = 50):
     Truncate a string to a maximum length with ellipsis if it's too long
     """
     if len(text) > length:
-        text = f"{text[:length - 3]}..."
+        text = f"{text[: length - 3]}..."
     return text
