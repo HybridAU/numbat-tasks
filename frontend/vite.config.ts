0e53d8e29201c5c348aa8fe8224cf2b8623cbd71
Michael Van Delft <michael@xo.tc>
2026-01-05 18:13:43 +0800

    Add icon files, and "latest" tag to releases

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,10 +2,6 @@ import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
 
-// TODO
-// * Generate icon files to match
-//
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
