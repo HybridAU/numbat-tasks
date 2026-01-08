94a4718fe1129bc663362d982f523bf969f8c764
Michael Van Delft <michael@xo.tc>
2026-01-09 06:51:05 +0800

    Better release - time to test it ans see what happens

--- utils/bump_version.py
+++ utils/bump_version.py
@@ -4,29 +4,42 @@ import re
 
 def calculate_new_version(version, increment):
     version_regex = re.compile(
-        r"^(?P<major>\d+)\.(?P<minor>\d+)\.(?P<patch>\d+)(?P<dev>(-dev)?)$"
+        r"^(?P<major>\d+)\.(?P<minor>\d+)\.(?P<patch>\d+)(?P<is_dev>-dev)?(?P<dev>\d*)$"
     )
     result = version_regex.search(version)
+
+    # Check for valid inputs
+    if increment not in ["major", "minor", "patch", "dev"]:
+        raise ValueError("Invalid increment")
+    if not result:
+        raise ValueError("Version number does not match the expected format")
+
     major = int(result.group("major"))
     minor = int(result.group("minor"))
     patch = int(result.group("patch"))
-    dev = bool(result.group("dev"))
+    is_dev = bool(result.group("is_dev"))
+    dev = result.group("dev")
+    # Dev is optional, so we check if it's an empty string before casting it as an int
+    dev = 0 if dev == "" else int(dev)
 
-    return_version = ""
+    calculated_version = ""
 
     if increment == "major":
-        return_version = f"{major + 1}.0.0"
+        calculated_version = f"{major + 1}.0.0"
     elif increment == "minor":
-        return_version = f"{major}.{minor + 1}.0"
+        calculated_version = f"{major}.{minor + 1}.0"
     elif increment == "patch":
-        if dev:
-            return_version = f"{major}.{minor}.{patch}"
+        if is_dev:
+            calculated_version = f"{major}.{minor}.{patch}"
         else:
-            return_version = f"{major}.{minor}.{patch + 1}"
+            calculated_version = f"{major}.{minor}.{patch + 1}"
     elif increment == "dev":
-        return_version = f"{major}.{minor}.{patch + 1}-dev"
+        if is_dev:
+            calculated_version = f"{major}.{minor}.{patch}-dev{dev + 1}"
+        else:
+            calculated_version = f"{major}.{minor}.{patch + 1}-dev"
 
-    return return_version
+    return calculated_version
 
 
 if __name__ == "main":
@@ -37,13 +50,10 @@ if __name__ == "main":
     )
     args = parser.parse_args()
 
-    if args.increment not in ["major", "minor", "patch", "dev"]:
-        raise Exception("Invalid increment")
-
     with open("VERSION") as file:
         current_version = file.read()
 
-    new_version = calculate_new_version(current_version)
+    new_version = calculate_new_version(current_version, args.increment)
 
     with open("VERSION", "w") as file:
         file.write(new_version)
